d6842e603fe1895e275e6b250eb70c28c6424d63
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-05-13 13:51:56 +0000

    Problems Complete

---
+++ src/main/java/Circle.java
@@ -0,0 +1,23 @@
+public class Circle{
+    protected double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double diameter(){
+        return this.radius*2;
+    }
+
+    public double perimeter(){
+        return this.radius * 2 * Math.PI;
+    }
+
+    public double area(){
+        return this.radius * this.radius * Math.PI;
+    }
+
+    public double getRadius(){return this.radius;}
+
+    public void setRadius(double radius){this.radius = radius;}
+}
